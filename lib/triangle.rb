48db0a9c3c68decfa16e5dc41017a97e16096b24
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-18 17:06:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,13 +10,17 @@ class Triangle
   
   def kind 
    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-     raise 
-    if @side_1 == @side_2 && @side_1 == @side_3
+     raise TriangleError
+   else if @side_1 + @side_2 < @side_3 || @side_2 + @side_3 < @side_1 || @side_1 + @side_3 < @side_2
+     raise TriangleError
+   else if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
     else if @side_1 == @side_2 && @side_2 != @side_3 || @side_2 == @side_3 && @side_1 != @side_2 || @side_1 == @side_3 && @side_1 != @side_2
       :isosceles
     else if @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
       :scalene
+    end
+    end
     end
     end
     end
