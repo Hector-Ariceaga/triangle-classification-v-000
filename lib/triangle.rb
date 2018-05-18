915944ab34fdfe71b890abe999e2c2fe74f44882
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-18 17:20:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,9 +3,7 @@ class Triangle
   attr_accessor :side_1, :side_2, :side_3
   
   def initialize(attributes)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+    
   end
   
   def kind 
