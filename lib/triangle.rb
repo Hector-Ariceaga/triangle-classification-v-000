e56d217b4dc8d69df43a9cb54086b2fc72b6f3b7
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-18 17:50:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       :equilateral
     else if side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       :isosceles
-    else if @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+    else
       :scalene
     end
     end
