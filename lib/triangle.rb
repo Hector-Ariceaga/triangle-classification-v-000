0d0f3bc612e82ab81faafcf6b7fd24891c2c0b96
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-05-18 16:59:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
   
   def kind 
-    
+    if 
     if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
     else if @side_1 == @side_2 && @side_2 != @side_3 || @side_2 == @side_3 && @side_1 != @side_2 || @side_1 == @side_3 && @side_1 != @side_2
